f69ebcbd8d5724c75a173ea6e7d51ee2e27a90f1
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-05-10 06:57:52 +0200

    setup app construct / add coinIcon

--- coin-flip/src/App.tsx
+++ coin-flip/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import coinIcon from "./assets/coin-icon.png";
 import "./App.css";
 
 function App() {
@@ -9,22 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={coinIcon} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Coin flip</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
