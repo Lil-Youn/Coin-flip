43729457ad6fe4fc64334f0a5bacb6e929fc7168
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-05-10 07:40:29 +0200

    add generic modal and close function

--- coin-flip/src/App.tsx
+++ coin-flip/src/App.tsx
@@ -1,30 +1,51 @@
-import { useState } from "react";
-
-import { Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import "./App.css";
 import coinIcon from "./assets/coin-icon.png";
+import GenericModal from "./components/generic-modal";
+import { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [modalOpenCoin, setModalOpenCoin] = useState(false);
   return (
     <>
-      <div>
-        <a>
-          <img src={coinIcon} className="logo " alt="logo" />
-        </a>
-      </div>
-      <Typography variant="h1">Coin flip</Typography>
-      <div className="card">
-        <Button
-          variant="contained"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-      </div>
-      <Typography variant="h5">Click on the Coin to start</Typography>
+      <Box>
+        <div>
+          <a>
+            <img src={coinIcon} className="logo " alt="logo" />
+          </a>
+        </div>
+        <Typography variant="h1">Coin flip</Typography>
+        <div className="card">
+          <Button onClick={() => setModalOpenCoin(true)} variant="contained">
+            Start game
+          </Button>
+        </div>
+        <Typography variant="h5">Click on the Coin to start</Typography>
+      </Box>
+      <CoinFlipGame
+        open={modalOpenCoin}
+        close={() => {
+          setModalOpenCoin(false);
+        }}
+      />
     </>
+  );
+}
+
+function CoinFlipGame({ open, close }: { open: boolean; close: () => void }) {
+  return (
+    <GenericModal header="Ready Player Nr.1" open={open} close={close}>
+      <div>
+        <IconButton
+          onClick={() => {
+            close();
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </div>
+    </GenericModal>
   );
 }
 
